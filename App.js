acddf4df19c3c70b6bdac827a00880a388dd0529
kenty99 <ec1131hk@ed.ritsumei.ac.jp>
2023-02-24 16:45:55 +0900

    first

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import "./style/App.css"
+import"./style/Header.css"
+import"./style/body.dss"
+
+
+
+const Header = () => {
+    return(
+       <div classname = "conponent_header">
+        <h1>たいとる</h1>
+        {/* ここに月の表示を入れるor headerを二つにわけていじる？ */}
+        </div>
+    )
+}
+
+const  body = () => {
+    return(
+
+    )
+}
